fa4ce40a4e1153fb9aed6bc97e0cfec1bb6ad6c9
galinafrost <morozgalina13@gmail.com>
2021-11-30 07:33:34 +0200

    правки

---
+++ js/modal-menu.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-mobil-menu-open]'),
+      closeModalBtn: document.querySelector('[data-mobil-menu-close]'),
+      modal: document.querySelector('[data-mobil-menu]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-visible');
+    }
+  })();
