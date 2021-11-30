956071f4021e8b04d17035d828f312475dfb04d3
galinafrost <morozgalina13@gmail.com>
2021-11-30 22:53:29 +0200

    модельное меню

--- js/modal-menu.js
+++ js/modal-menu.js
@@ -1,14 +1,14 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-mobil-menu-open]'),
-      closeModalBtn: document.querySelector('[data-mobil-menu-close]'),
-      modal: document.querySelector('[data-mobil-menu]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-visible');
-    }
-  })();
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
